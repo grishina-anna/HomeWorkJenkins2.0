2a41cd281c5fdeff4bc5e60168ff605c5fad53fe
grish <grishina@factor-ts.ru>
2022-01-25 22:49:58 +0300

    с /wd/hub

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -23,7 +23,7 @@ public class TestBase {
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", false);
         Configuration.browserCapabilities = capabilities;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud"; //для автотестов в хроме
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub"; //для автотестов в хроме
     }
 
     @AfterEach
