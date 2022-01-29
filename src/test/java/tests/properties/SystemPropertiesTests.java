3a529dbcf08dc9a3a07f22df50e731b10fbfec0c
grish <grishina@factor-ts.ru>
2022-01-29 17:20:54 +0300

    добавлены системные свойства

--- src/test/java/tests/properties/SystemPropertiesTests.java
+++ src/test/java/tests/properties/SystemPropertiesTests.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Configuration.browser;
 
 public class SystemPropertiesTests {
-//        System.getProperties("key"); - системные хранилища в памяти проекта
+    //        System.getProperties("key"); - системные хранилища в памяти проекта
 //        System.setProperties("key", "value"); - системные хранилища в памяти проекта
     @Test
     void someTest1() {
@@ -14,15 +14,15 @@ public class SystemPropertiesTests {
         System.out.println(someValue); //null
     }
 
-        @Test
-        void someTest2(){
-            System.setProperty ("someKey", "red value");
-            String someValue = System.getProperty("someKey");
-            System.out.println(someValue); //red value
-        }
+    @Test
+    void someTest2() {
+        System.setProperty("someKey", "red value");
+        String someValue = System.getProperty("someKey");
+        System.out.println(someValue); //red value
+    }
 
     @Test
-    void someTest3(){
+    void someTest3() {
         String someValue = System.getProperty("someKey", "blue value"); //если по someKey ничего не нашлось, то ему установят дефолтное значение
         System.out.println(someValue); //red value
     }
@@ -34,18 +34,17 @@ public class SystemPropertiesTests {
         System.out.println(browser);
     }
 
-    Test
+    @Test
     @Tag("properties")
     void someTest5() {
-        String browser = System.getProperty("browser","chrome"); //по дефолту будет хром
-        String version = System.getProperty("version","91");
-        String browserSize = System.getProperty("browserSize","300x300");
+        String browser = System.getProperty("browser", "chrome"); //по дефолту будет хром
+        String version = System.getProperty("version", "91");
+        String browserSize = System.getProperty("browserSize", "300x300");
 
         System.out.println(browser);
         System.out.println(version);
         System.out.println(browserSize);
     }
 
-    }
-
 }
+
