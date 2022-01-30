fb22e4a84428641c9a348e3548323677eee0c467
grish <grishina@factor-ts.ru>
2022-01-30 21:06:51 +0300

    добавлены данные для удаленного сервера

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,8 +2,6 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.SelenideConfig;
-import com.codeborne.selenide.SelenideDriver;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
@@ -16,19 +14,33 @@ public class TestBase {
 
     @BeforeAll
     static void setUp() {
-
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
+        String browser = System.getProperty("browser", "chrome");
+        String version = System.getProperty("version", "91");
+        String remoteUrl = System.getProperty("remoteUrl", "selenoid.autotests.cloud/wd/hub1");
+        String login = System.getProperty("login", "user11");
+        String pass = System.getProperty("pass", "12341");
 
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        Configuration.browser = browser;
+        Configuration.browserVersion = version;
 
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+   //     Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        String url = "https://" + login + ":" + pass + "@" + remoteUrl;
+
+        Configuration.remote = url;
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
+
+        Attach.attachAsText("Browser: ", browser);
+        Attach.attachAsText("Version: ", version);
+        Attach.attachAsText("Remote Url: ", remoteUrl);
     }
 
     @AfterEach
