c9cb86987149a29e4e33fe138092b9d1ea7f475a
grish <grishina@factor-ts.ru>
2022-02-24 17:15:07 +0300

    добавлены данные для удаленного сервера

---
+++ src/test/java/tests/owner/OwnerTests.java
@@ -0,0 +1,25 @@
+package tests.owner;
+
+import config.CredentialsConfig;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Test;
+
+import static java.lang.String.format;
+
+public class OwnerTests {
+
+    CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
+
+
+    @Test
+    void loginTest(){
+       String login = config.login();
+        String password = config.password();
+
+        System.out.println("Login: " + login);
+        System.out.println("Password: " + password);
+
+        String message = format ("I logger in as %s with password %s", login, password);
+        System.out.println(message);
+    }
+}
