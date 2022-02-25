e5444f5b32eb65a867d602be5966fe9e417ba557
grish <grishina@factor-ts.ru>
2022-02-25 14:22:16 +0300

    создана ветка owner

--- src/test/java/tests/demo/qa/TestBase.java
+++ src/test/java/tests/demo/qa/TestBase.java
@@ -1,9 +1,10 @@
 package tests.demo.qa;
 
 import com.codeborne.selenide.Configuration;
-import config.Credentials;
+import config.CredentialsConfig;
 import helpers.Attach;
 import io.qameta.allure.Step;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -13,30 +14,32 @@ import static java.lang.String.format;
 
 public class TestBase {
     @BeforeAll
-    @Step("Конфигурация параметров запуска тестов")
     static void setUp() {
 
-        String login = Credentials.config.login();
-        String password = Credentials.config.password();
+        CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
+
+        String browser = System.getProperty("browser");
+        String version = System.getProperty("version");
+        String login = config.login();
+        String password = config.password();
         String url = System.getProperty("url");
         String remoteUrl = "https://" + login + ":" + password + "@" + url;
 
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browserVersion", "91");
-        Configuration.browserSize = System.getProperty("browserSize", "1024x768");
+        Configuration.browserSize = "1920x1080";
+        Configuration.browser = browser;
+        Configuration.browserVersion = version;
         Configuration.remote = remoteUrl;
-        //Configuration.remote = format("https://%s:%s@%s", login, password, remoteUrl);
-        //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
-        Configuration.browserCapabilities = capabilities;    }
+        Configuration.browserCapabilities = capabilities;
+    }
+
     @AfterEach
     void addAttachments() {
-        Attach.screenshotAs("Last screenshot");
+        Attach.screenshotAs(System.getProperty("browser")+" "+System.getProperty("version"));
         Attach.pageSource();
         Attach.browserConsoleLogs();
         Attach.addVideo();
